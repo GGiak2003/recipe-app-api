0555409e184631e67d0ee3b0aba5ee32d9735384
Giacomo Bellomo <giacomo.bellomo03@gmail.com>
2025-01-09 15:42:05 +0100

    test

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
@@ -15,7 +16,7 @@ class CalcTests(SimpleTestCase):
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """Test subracting numbers."""
+        """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
