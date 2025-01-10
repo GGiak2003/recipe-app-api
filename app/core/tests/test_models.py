db554af4ea7974f135385d3650bbe93a7e716afd
Giacomo Bellomo <giacomo.bellomo03@gmail.com>
2025-01-10 15:21:40 +0100

    test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,19 +9,19 @@ class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with email is successful."""
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
-            password=password
+            password=password,
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test email is normalized for new user."""
+        """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
