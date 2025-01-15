f6759911c88c8fe28a9d62a31027f9e88a38b83d
Giacomo Bellomo <giacomo.bellomo03@gmail.com>
2025-01-15 10:26:55 +0100

    test

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -101,6 +101,8 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_user_with_blank_password(self):
+        """Test posting a blank password returns an error."""
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
