7789429b73b11329a9cd0e1dbd3d9ccba0d74f81
Giacomo Bellomo <giacomo.bellomo03@gmail.com>
2025-01-22 16:49:57 +0100

    m

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -54,18 +54,17 @@ class RecipeSerializer(serializers.ModelSerializer):
     def _get_or_create_ingredients(self, ingredients, recipe):
         """Handle getting or creating ingredients as needed."""
         auth_user = self.context['request'].user
-        for ingrediet in ingredients:
-            ingredient_obj, create = Ingredient.objects.get_or_create(
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
                 user=auth_user,
-                **ingrediet,
+                **ingredient,
             )
-            recipe.ingredient.add(ingredient_obj)
-
+            recipe.ingredients.add(ingredient_obj)
 
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
-        ingredients = validated_data.pop('ingredient', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
         self._get_or_create_ingredients(ingredients, recipe)
@@ -75,9 +74,15 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
+
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
